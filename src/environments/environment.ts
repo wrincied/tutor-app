0b58fff2aae0947d9bf7ca5ee2cccee62cc4b351
wrincied <wrincied@gmail.com>
2026-05-23 20:42:15 +0200

    Commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,25 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3001',
-  /** URL SPA для Firebase Email Verification (actionCodeSettings). */
-  appUrl: 'http://localhost:4200',
-  firebase: {
-    apiKey: 'AIzaSyDI_O1K8uBbEIe2uJvY-8tzpJjfhgg0rlw',
-    authDomain: 'tutorassis.firebaseapp.com',
-    projectId: 'tutorassis',
-    storageBucket: 'tutorassis.firebasestorage.app',
-    messagingSenderId: '965411338053',
-    appId: '1:965411338053:web:d98317afdb5602a53c702c',
-    measurementId: 'G-TXM74NGN6R',
-  },
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:3001',
+//   /** URL SPA для Firebase Email Verification (actionCodeSettings). */
+//   appUrl: 'http://localhost:4200',
+//   firebase: {
+//     apiKey: 'AIzaSyDI_O1K8uBbEIe2uJvY-8tzpJjfhgg0rlw',
+//     authDomain: 'tutorassis.firebaseapp.com',
+//     projectId: 'tutorassis',
+//     storageBucket: 'tutorassis.firebasestorage.app',
+//     messagingSenderId: '965411338053',
+//     appId: '1:965411338053:web:d98317afdb5602a53c702c',
+//     measurementId: 'G-TXM74NGN6R',
+//   },
+// };
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDSD3tq-29elNYooYNWVsx5pwJH8TqiqO4",
+  authDomain: "simple4u-f5e6e.firebaseapp.com",
+  projectId: "simple4u-f5e6e",
+  storageBucket: "simple4u-f5e6e.firebasestorage.app",
+  messagingSenderId: "1000505379799",
+  appId: "1:1000505379799:web:b1010f295eda80fe5b5992",
+  measurementId: "G-GR8Q29PW4R"
 };
